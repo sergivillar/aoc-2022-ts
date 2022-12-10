777d9a704ca45bf11be9afdecb59e2706d866ba7
Sergio Fernandez Jimenez <sergio.fernandezjimenez@telefonica.com>
2022-12-10 11:07:18 +0100

    solve day 10

--- day10/1.ts
+++ day10/1.ts
@@ -1,5 +1,29 @@
 import { readFile } from "../utils/read-file";
 
-const file = readFile(__dirname + "/input.txt");
+const file = readFile(__dirname + "/input.txt").split("\n");
 
-console.log("Result -> ");
+let cycleCount = 0;
+let x = 1;
+let total = 0;
+
+const FREQUENCIES = [20, 60, 100, 140, 180, 220];
+
+const checkIterarion = () => {
+  if (FREQUENCIES.includes(cycleCount)) {
+    total += x * cycleCount;
+  }
+};
+
+file.forEach((line) => {
+  cycleCount += 1;
+  checkIterarion();
+
+  if (line !== "noop") {
+    const value = line.split(" ")[1] ?? 0;
+    cycleCount += 1;
+    checkIterarion();
+    x += Number(value);
+  }
+});
+
+console.log("Result -> ", total);
--- day10/2.ts
+++ day10/2.ts
@@ -1,5 +1,42 @@
 import { readFile } from "../utils/read-file";
 
-const file = readFile(__dirname + "/input.txt");
+const file = readFile(__dirname + "/input.txt").split("\n");
 
-console.log("Result -> ");
+const WIDE = 40;
+const HIGH = 6;
+const LIT = "#";
+const DARK = ".";
+
+const CRT: Array<Array<string | null>> = Array.from({ length: HIGH }, () =>
+  Array.from({ length: WIDE }, () => null)
+);
+
+let cycleCount = 0;
+let spritePosition = 1;
+
+const draw = () => {
+  const tmp = cycleCount - 1;
+  const col = tmp % WIDE;
+  const row = parseInt(String(tmp / WIDE));
+
+  const draw =
+    col >= spritePosition - 1 && col <= spritePosition + 1 ? LIT : DARK;
+
+  CRT[row][col] = draw;
+};
+
+file.forEach((line) => {
+  cycleCount += 1;
+  draw();
+
+  if (line !== "noop") {
+    const value = line.split(" ")[1] ?? 0;
+    cycleCount += 1;
+    draw();
+    spritePosition += Number(value);
+  }
+});
+
+CRT.forEach((line) => {
+  console.log(line.map((i) => i).join(""));
+});
